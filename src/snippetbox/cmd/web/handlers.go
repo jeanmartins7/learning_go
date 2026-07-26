542780ed764b6e297cddddae8166198576d18dc7
jeanverso <jeanmarcel8@gmail.com>
2026-07-26 16:08:24 -0300

    chore: refactory

--- src/snippetbox/cmd/web/handlers.go
+++ src/snippetbox/cmd/web/handlers.go
@@ -17,11 +17,11 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Server", "Go")
 
 	files := []string{
-		"./ui/html/base.tmpl",
-		"./ui/html/partials/nav.tmpl",
-		"./ui/html/pages/home.tmpl",
-		"./ui/html/pages/title.tmpl",
-		"./ui/html/pages/main.tmpl",
+		"../../ui/html/base.tmpl",
+		"../../ui/html/partials/nav.tmpl",
+		"../../ui/html/pages/home.tmpl",
+		"../../ui/html/pages/title.tmpl",
+		"../../ui/html/pages/main.tmpl",
 	}
 
 	ts, err := template.ParseFiles(files...)
